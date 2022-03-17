7606cf09f6c704bb47c1784f5222548959c106fe
Timothy Simon Sihombing <tmsimon29@gmail.com>
2022-03-17 22:53:23 +0700

    initial luas

---
+++ luas.py
@@ -0,0 +1,9 @@
+def hitung_luas_lingkaran(r): 
+    luas = 3.14 * r**2 
+    return luas
+def hitung_luas_persegi(s): 
+    luas = s * s 
+    return luas
+ 
+print ('Luas lingkaran dengan diameter 7 adalah ', hitung_luas_lingkaran(7)) 
+print ('Luas persegi dengan panong sIsi 4 adalah ', hitung_luas_persegi(4))
